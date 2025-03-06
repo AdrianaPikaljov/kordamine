45615c99f8df72e770ab50c07332c4e2031942d2
adri <pikaljovadri@gmail.com>
2025-03-06 14:44:39 +0200

    Update kordamine.py

--- kordamine.py
+++ kordamine.py
@@ -1,6 +1,12 @@
+#for – повторяет что-то определенное количество раз.
+#if – проверяет условие.
+#else – выполняет код, если if не сработал.
+#while – повторяет код, пока условие истинное.
+#try-except – ловит ошибки, чтобы программа не ломалась.
+#while True – бесконечный цикл, который нужно прерывать (break).
+
 #3 variant ülesanne 1
 n = int(input("sisesta mittu korda korrata (1-9): "))
-
 if n < 1 or n > 9:# kontrollime kas arv on õige
     print("viga, sisesta arv 1 - 9")
 else:
@@ -24,10 +30,10 @@ try:
         print("viga")
     else:
         maxnum = n ** 3 #predel otsime
-        i = 1 #aluste 1
+        i = 1 #alustame 1
         while i **aste <= maxnum: #kuni i ne bydet aste <= n3
             print(f"{i}**{aste} = {i **aste}")
-            i += 1
+            i += 1 #suurendame i
 except:
     print("viga")
 
@@ -36,14 +42,14 @@ print("")
 import random
 
 opilased = random.randint(5, 30) #gen arv
-sumhinn =0 #muutuja hinnete summa
+sumhinn =0 #muutuja hinnete summa selleks et summa tulemust salvestada
 
 for i in range(opilased):
     hinne = random.randint(1, 100) #gen hinne
     sumhinn += hinne #lisame hinne hinnete summale
 
 kesk= sumhinn / opilased #keskhinne
-print(f"opilasi on {opilased} ")
+print(f"opilasi on {opilased} ja hinded {hinne} ")
 print("keskhinne on:",kesk)
 
 
